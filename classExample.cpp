3a1f800990a62e9f3383a84ed63a29c06fad615b
Corey <1500812@abertay.ac.uk>
2019-02-15 00:40:06 +0000

    Added the file handling section
    The following addition includes a Class for managing a file to be read in chunks and a cpp main that makes use of it as an example case

---
+++ classExample.cpp
@@ -0,0 +1,44 @@
+#include "fileHandler.h"
+
+int main(int argc, char** argv)
+{
+#if DEBUG == true
+	cout << "DEBUG: We are currently in path" << endl << "DEBUG: ";
+
+	#ifdef _WIN32
+		system("cd");
+	#endif //_WIN32
+
+	#ifdef __unix__
+		system("pwd");
+	#endif //__unix__
+
+		cout << endl << endl;
+#endif // DEBUG
+
+	// Init the file handler class
+	fileHandler test("TestFile.test");
+
+	// Looped the chunk reading for some examples
+	for (int i = 0; i < 2; i++)
+	{
+		test.readNextChunk();
+		cout << test.buffer;
+	}
+	
+	// reseting the pointer to the start of the file
+	test.resetPointer();
+
+	// setting the chunk to one in
+	if (!test.setCurrChunkNo(1))
+	{
+		cerr << "That didn't work as planned";
+		return 1;
+	}
+
+	// reading that chunk and writing to console
+	test.readNextChunk();
+	cout << test.buffer;
+
+	return 0;
+}
---
+++ fileHandler.cpp
@@ -0,0 +1,125 @@
+#include "fileHandler.h"
+
+
+//fileHandler::fileHandler(string fileName, size_t currChunkSize = 20MB)
+fileHandler::fileHandler(string fileNameGiven, size_t currChunkSize)
+{
+	fileName = fileNameGiven;  // Set this for the debug messages in future
+	chunkSize = currChunkSize; // By having a default value we can change it easier for testing
+
+	// Open the file requested and since this is a C method it doesn't like strings just char arrays
+	#pragma warning(suppress : 4996)
+	fileToCarve = fopen(fileName.c_str(), "rb");
+
+	// Checking the file opened worked before setting buffers in the event an error occurs
+	if (fileToCarve == NULL) // If file didn't open
+	{
+		cerr << "File Error with: " << fileName << endl
+			 << "Does this file exitst here?" << endl;
+
+	#if defined(_WIN32)
+		system("cd");
+	#elif defined(__unix__)
+		system("ls");
+	#endif // _WIN32
+		exit(1);
+	}
+
+	// This may need done again later so is kept to a function
+	checkFileSize();
+
+	// Setup buffers - Due to the use of C methods and the size of the file chunks
+	// sticking to the methods demonstrated in the examples seemed the best approach
+	// compared to trying to load the files into CPP char arrays.
+	buffer = (char*)malloc(sizeof(char)*chunkSize);
+	if (buffer == NULL)
+	{
+		cerr << "Memory Allocation Error for chunks of size " << chunkSize << "Bytes";
+		exit(2);
+	}
+
+	// Good to have handy in a variable
+	totalChunks = ceil(fSize/chunkSize);
+	// Remainder is still a chunk. Just the GPU's problem of how to propogate that throughout
+
+	return;
+}
+
+long fileHandler::checkFileSize()
+{
+	rewind(fileToCarve);
+	fseek(fileToCarve, 0, SEEK_END);
+	fSize = ftell(fileToCarve);
+
+	#if DEBUG == true
+	cerr << fileName << " is of size: " << fSize << " Bytes" << endl << endl;
+	#endif 
+
+	rewind(fileToCarve);
+	currChunk = 0;
+	return fSize;
+}
+
+void fileHandler::resetPointer()
+{
+	rewind(fileToCarve);
+	currChunk = 0;
+	return;
+}
+
+void fileHandler::confirmFileSize()
+{
+	long fileSizeBefore = fSize;
+
+	if (fileSizeBefore != checkFileSize())
+	{
+		cerr << "The file has been altered in some way. This is not the file from before; exiting";
+		free(buffer); // To be safe
+		exit(3);
+	}
+}
+
+void fileHandler::readNextChunk()
+{
+	fread(buffer, chunkSize, 1, fileToCarve);
+	return;
+}
+
+double fileHandler::getTotalChunks()
+{
+	return totalChunks;
+}
+
+long fileHandler::getCurrChunkNo()
+{
+	return currChunk;
+}
+
+bool fileHandler::setCurrChunkNo(long newChunkNo)
+{
+	if (newChunkNo >= getTotalChunks())
+	{
+		return false;
+	}
+
+	// Set the file cursor to the newChunkNo chunks into the file
+	if (EXIT_SUCCESS == fseek(fileToCarve, newChunkNo*chunkSize, SEEK_SET))
+	{
+		currChunk = newChunkNo;
+	} else { // If the file was not repointed correctly
+		
+		cerr << "There was an issue repointing to position " << newChunkNo <<
+			" the pointer was reset to the start as a result and execution has continued" << endl;
+		resetPointer();
+		return false;
+	}
+
+	return true;
+}
+
+
+fileHandler::~fileHandler()
+{
+	free(buffer);
+	return;
+}
